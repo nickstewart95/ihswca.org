73822375032f86effded1b4bbc4238494c89e1aa
Nick Stewart <nick@nickstewart.me>
2024-01-12 12:24:30 -0500

    feat: Styled frontpage

---
+++ frontpage.php
@@ -0,0 +1,64 @@
+<?php
+get_header(); ?>
+
+<div class="section section-alt homepage-intro">
+	<h2>Growing Indiana Wrestling</h2>
+	<p>
+		The Indiana High School Wrestling Coaches Association was established
+		to stimulate and promote the growth of wrestling in Indiana schools.
+		The Association also promotes sportsmanship and a friendly
+		understanding among all schools that conduct wrestling. The IHSWCA
+		works to affirm our belief in the Indiana High School Athletic
+		Association and support its principles.
+	</p>
+</div>
+<div class="section the-numbers">
+	<h2>The Numbers</h2>
+	<div class="numbers">
+		<div class="box">
+			<h3>115<span> Member Coaches</span></h3>
+			<p>
+				With expertise and a passion for the sport, our coaches pave the way for wrestling excellence and empower student athletes to reach their full potential.
+			</p>
+		</div>
+		<div class="box">
+			<h3>1,610+ <span> Represented Athletes</span></h3>
+			<p>
+				Meet Indiana's extraordinary student athletes, the high school wrestlers who excel both on the mat and in the classroom. These dedicated individuals showcase remarkable skill, resilience, and a relentless pursuit of excellence, setting the standard for sportsmanship and academic achievement.
+			</p>
+		</div>
+	</div>
+</div>
+<div class="section section-alt">
+	<h2>Academic All State</h2>
+	<p>
+		Celebrate excellence both on the mat and in the classroom with the prestigious Indiana Wrestling Academic All-State Award! Recognizing the dedication and hard work of student-athletes who excel in wrestling while maintaining outstanding academic performance, this award embodies the perfect balance of athletic prowess and scholastic achievement. Join us in honoring these remarkable individuals whose commitment to both sport and academics sets them apart as shining examples of achievement and dedication.
+	</p>
+	<p>
+		For more information, go to
+		<a href="/academic-all-state">https://ihswca.org/academic-all-state</a>
+	</p>
+</div>
+<div class="section">
+	<h2>Middle School State</h2>
+	<p>
+		Step onto the mat and witness the future of wrestling at the Indiana Wrestling Middle School State Tournament! A thrilling showcase of young talent and determination, this tournament is where budding athletes from across the state converge to compete, showcasing their skills, passion, and dedication to the sport. Join us in celebrating the resilience and spirit of these young athletes as they take center stage in this exhilarating competition!
+	</p>
+	<p>
+		For more information, go to
+		<a href="/middle-school-state">https://ihswca.org/middle-school-state</a>
+	</p>
+</div>
+<div class="section section-alt">
+	<h2>Team State</h2>
+	<p>
+		With a rigorous selection process that honors the best of the best, this event brings together the top 12 schools from each of the 3A, 2A, and 1A classes and the top eight schools from the 4A class. From the meticulously chosen top teams from the IHSAA individual semi-state locations to the wildcard entries, every participant embodies the spirit of excellence. 
+	</p>
+	<p>
+		For more information, go to
+		<a href="/team-state">https://ihswca.org/team-state</a> or join the dicussion on
+		<a href="https://indianamat.com/index.php?/forums/topic/62913-team-state-qualification-leaderboard-2023-final/">Indiana Mat</a>.
+	</p>
+</div>
+
+<?php get_footer(); ?>
