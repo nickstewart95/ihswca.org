7aa031001801ddd449fe237fd663a2ef1f3cbbbf
Nick Stewart <nick@nickstewart.me>
2024-01-12 12:31:14 -0500

    feat: Styled 404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,9 @@
-404.php
+<?php
+get_header(); ?>
+<div class="section section-alt homepage-intro">
+    <h2>Page Not Found</h2>
+</div>
+<div class="section page-content">
+    <p>The content you are looking for could not be found.</p>
+</div>
+<?php get_footer();
