44b89804d311107bab8dccc50eb9c497d70fd1b8
Nick Stewart <nick@nickstewart.me>
2024-01-12 12:27:43 -0500

    feat: Styled pages/posts

--- single.php
+++ single.php
@@ -1,1 +1,15 @@
-single.php
+<?php
+get_header();
+
+if (have_posts()) {
+	while (have_posts()) {
+		the_post(); ?>
+			<div class="section section-alt homepage-intro">
+				<h2><?php the_title(); ?></h2>
+				<?php echo get_field('intro_text', get_the_ID()); ?>
+			</div>
+			<div class="section page-content"><?php echo the_content(); ?></div>
+<?php
+	}
+}
+get_footer();
